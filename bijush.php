5aed190a61be68784cd6b9065c7ede3b0f49d03b
Bijush <38435589+Bijush@users.noreply.github.com>
2019-06-23 16:25:35 +0530

    bijush.php

---
+++ bijush.php
@@ -0,0 +1,257 @@
+<!doctype html> 
+<html lang="en"> 
+<head> 
+<!-- Required meta tags -->
+ <meta charset="utf-8"> 
+ <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> <!-- Bootstrap CSS --> 
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+   <link rel="stylesheet" href="biju.css">
+  <title>Bijush Kumar Roy</title> 
+ </head> 
+ 
+ <body onload="load()"> 
+ <div class="jumbotron img-class">
+ <div class="container-fluid main">
+ <div class="row mrow">
+ <div class="col-sm-9">
+ 
+ <img src="image/Biju.jpg" class="img-fluid" alt="biju" />
+ </div>
+ <div class="col-sm-3 H6 text-center">
+ <h6 class="text-center">Bijush Kumar Roy</h6><br>
+ 
+ </div>
+ </div>
+ </div>
+ </div>
+ <!-- First container end-->
+ 
+ <div class= "container-fluid navcon">
+ <div class="row navcon">
+ <div class="col-sm-12">
+<nav class="navbar navbar-light" style="background-color: #e3fffd;"> <!-- Navbar content -->
+<a class="navbar-brand" href="#">MENU</a>
+ <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+ <span class="navbar-toggler-icon">
+ </span> 
+ </button> 
+ <div class="collapse navbar-collapse" id="navbarNav"> 
+ <ul class="navbar-nav"> 
+ <li class="nav-item active"> 
+ <a class="nav-link" href="#">Home
+ </a></li> 
+ <li class="nav-item">
+ <a class="nav-link" href="gk.php">Gk</a></li> 
+ <li class="nav-item">
+ <a class="nav-link" href="#">Asos
+ </a> </li> 
+ <li class="nav-item">
+ <a class="nav-link" href="#">H.S.L.C</a>
+ </li> </ul></div>
+</nav>
+ </div>
+</div>
+</div>
+</div>
+span<span class="glyphicon glyphicon-warning-sign b"></span>
+ <!--- nav end above & container-->
+ <!---Card Start From Here-->
+ <div class="jumbotron img-class">
+ <form action="bijushLogin.php">
+ <div class="form-group">
+ <input type="submit" class="btn btn-primary form-control" value="Login"></div>
+</form>
+<form action="bijushsignup.php">
+<div class="form-group">
+ <input type="submit" class="btn btn-primary form-control" value="Create Account"></div>
+</form>
+
+ <div class="row"> 
+ <div class="col-sm-3"> 
+ <div class="card w-18rem">
+ <video width="100%" height="80%" class="video" controls>
+<source src="video.mp4" type="video/mp4"></video>
+ <!--<img src="image/Biju.jpg" class="card-img-top" alt="B">-->
+ <div class="card-body"> 
+ <h5 class="card-title">News Bangla</h5> 
+ <!--<p class="card-text">With supporting text below as a natural lead-in to additional content.</p> -->
+ <a href="#" class="btn btn-primary">Download</a> 
+ </div> 
+ </div> 
+ </div>
+<!--col end-->
+
+ <div class="col-sm-3">
+ <div class="card w-18rem">
+ <video width="100%" height="80%"  class="video" controls>
+ <source src="videos/B.mp4" type="video/mp4"></video>
+ <!--<img src="image/Biju.jpg" class="card-img-top" alt="A">-->
+ <div class="card-body">
+ <h5 class="card-title"> Nature</h5>
+ <!--<p class="card-text"> Card TextWith supporting text below as a natural lead-in to additional content. Her-->
+ <a href="#" class="btn btn-primary">Download</a>
+ </div>
+ </div>
+ </div>
+ <div class="col-sm-3">
+ <div class="card w-80">
+ <video width="100%" height="80%" controls>
+ <source src="video.mp4" type="video/mp4">
+ </video>
+ <div class="card-body">
+ <h5 class="title-text"> News Borak
+ </h5>
+ </div>
+ </div>
+ </div>
+ <div class="col-sm-3">
+ <div class="card w-18rem">
+ <video width="100%" height="80%" controls>
+ <source src="video.mp4" type="video/mp4"></video>
+ <div class="card-body">
+ <h5 class="title-text">new video
+ </h5>
+ </div>
+ </div>
+ </div>
+ </div>
+ </div>
+ <!-- Card End Here-->
+ 
+ <div class="jumbotron">
+ <div class="row">
+ <div class="col-sm-12 menu">
+ <ul>
+ <a href="#"><li inline>Image</li></a>
+ <a href="#"><li>Music</li></a>
+ <a href="video.php"><li>Videos</li></a>
+ <a href="#"><li>Dance</li></a>
+ </ul>
+ </div>
+ </div>
+ </div>
+ <!--2nd container-fluid end-->
+ <!-- third body content fluid start --><br>
+ <div class="container-fluid">
+ <div class="row v">
+ <div class="col-sm-6">
+ <ul class="list-instyled">
+ <li class="media">
+ <img src="image/Biju.jpg" height=80px width=100px alt="biju" class="mr-3 align-self-start img-fluid">
+ <div class="media-body">
+ <h5> About Me</h5>
+ <p class="f"> Hii ....I am Bijush Kumar Roy.<br> I'm a teacher and also a software engineer.<br> I have a institute for teaching web developing course.<br> if you are interested for learning software engineering to can to join us.<br>
+ <strong> Our Address :</strong><br>
+ Chibita Bichia <br>
+ Slichar(Assam).
+ 
+ </p>
+ </div>
+ </li>
+ </ul>
+ </div>
+ <div class="col-sm-6">
+  <ul class="list-instyled">
+ <li class="media">
+ <img src="image/B.jpg" height=80px width=100px alt="biju" class="mr-3 align-self-start img-fluid">
+ <div class="media-body">
+ <h5> About Me</h5>
+ <p class="f"> Hii ....I am Bijush Kumar Roy.<br> I'm a teacher and also a software engineer.<br> I have a institute for teaching web developing course.<br> if you are interested for learning software engineering to can to join us.<br>
+ <strong> Our Address :</strong><br>
+ Chibita Bichia <br>
+ Slichar(Assam).
+ 
+ </p>
+ </div>
+ </li>
+ </ul>
+ </div>
+ </div>
+ </div>
+ <div class="container">
+ <form>
+ <div class="form group">
+ <h4 class="form-control"> How many districts in India?
+ </h4>
+ <h6> 723</h6>
+ <div class="jumbotron" id="hide">
+ <h6 id="show">Your Answer is Corrects</h6>
+ </div>
+<button type="button" class="btn btn-primary" id="btn1"> Show answer</button>
+ 
+ 
+ </form>
+ </div>
+ </div>
+
+ <!-- pagination section start-->
+ <br>
+ <div class="jumbotron">
+ <div class="container">
+ <nav> 
+ <ul class="pagination pagination-lg"> 
+ <li class="page-item active">
+ <a class="page-link" href="#">1</a>
+ </li> 
+ <li class="page-item">
+ <a class="page-link" href="bk.php">2</a></li> 
+ <li class="page-item">
+ <a class="page-link" href="#">3</a>
+ </li> 
+ <li class="page-item">
+ <a class="page-link" href="#">Next</a> </li></ul> </nav>
+ </div>
+ </div>
+ <!--page section End-->
+ <br>
+ <p id="alert">
+ <!--<//?php
+ echo "you are welcome".$_SESSION['user'];
+ ?>-->
+ </p>
+ <?php
+ echo "This is Php Code"."<br>"	;
+ echo $_SERVER['HTTP_HOST']."<br>";
+echo $_SERVER['HTTP_USER_AGENT']."<br>";
+echo $_SERVER['SERVER_NAME	']."<br>";
+echo $_SERVER['SERVER_ADDR']."<br>";
+ echo $_SERVER['PHP_SELF']."<br>";
+$file=fopen();
+ ?>
+<div class="form-control">
+  <button type="button" data-toggle="popover" title="Popover Header" data-content="Some content inside the popover">Toggle popover
+</button>
+</div>
+</div>
+
+<script>
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover();   
+});
+</script>
+ 
+ <!--<button type="button" class="btn btn-lg btn-danger" data-toggle="popover" title="Popover title" data-content="And here's some amazing content. It's very engaging. Right?">Click to toggle popover</button>
+
+<script>$(function () { $('.example-popover').popover({ container: 'body' }) })</script>-->
+ 
+ <img src="http://i65.tinypic.com/28in3td.png">
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script> <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+ 
+ 
+ 
+ <!-- Optional JavaScript --> 
+ <!-- jQuery first, then Popper.javascript-->
+
+<!--- <script type="text/javascript" src="jquery.js"></script>
+  <script type="text/javascript" src="my.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>-->
+  </body> </html>
